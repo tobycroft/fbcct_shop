aa936133f169aa574b62ed8d4e6d23a97ba6dc42
Tuuz <silverhawk@foxmail.com>
2021-11-23 17:13:30 +0800

    TuuzMac

--- app/api/controller/wap/Users.php
+++ app/api/controller/wap/Users.php
@@ -184,6 +184,7 @@ class Users extends Base
 
     public function register()
     {
+        return $this->returnAjax("请使用fbcct秘钥登录/恢复");
         $username = Request::param("username", "", "trim,strip_tags");
         $password = Request::param("password", "", "trim,strip_tags");
         $code = Request::param("code", "", "intval");
@@ -265,6 +266,7 @@ class Users extends Base
 
     public function forget()
     {
+        return $this->returnAjax("请使用fbcct秘钥登录/恢复");
         $username = Request::param("username", "", "trim,strip_tags");
         $password = Request::param("password", "", "trim,strip_tags");
         $code = Request::param("code", "", "intval");
@@ -327,7 +329,7 @@ class Users extends Base
 
     public function send_sms()
     {
-        return $this->returnAjax("请使用fbcct秘钥登录");
+        return $this->returnAjax("请使用fbcct秘钥登录/恢复");
         $username = Request::param("username", "", "trim,strip_tags");
         $type = Request::param("type", "", "trim,strip_tags");
 
