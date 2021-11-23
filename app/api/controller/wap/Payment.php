658aee2009d14eb6f026ead310a933bcaa17998b
Tuuz <silverhawk@foxmail.com>
2021-11-23 15:12:00 +0800

    TuuzMac

--- app/api/controller/wap/Payment.php
+++ app/api/controller/wap/Payment.php
@@ -27,7 +27,7 @@ class Payment extends Base
             $in[] = "common";
         }
 
-        $res = Db::name("payment")->where("status", "0")->where("type", "in", $in)->order("sort", "ASC")->select()->toArray();
+        $res = Db::name("payment")->where("status", "0")->where("type", "in", $in)->order("sort", "DESC")->select()->toArray();
 
         $array = [];
         $style = [
