9d0cd6cf1d70ddfeb579f9108da22b7a2bdbb9cc
Tuuz <silverhawk@foxmail.com>
2021-11-29 16:18:07 +0800

    TuuzMac

--- vendor/lcobucci/jwt/src/Signer/Key/LocalFileReference.php
+++ vendor/lcobucci/jwt/src/Signer/Key/LocalFileReference.php
@@ -8,7 +8,6 @@ use function file_exists;
 use function strpos;
 use function substr;
 
-/** @deprecated Use \Lcobucci\JWT\Signer\Key\InMemory::file() instead */
 final class LocalFileReference extends Key
 {
     const PATH_PREFIX = 'file://';
@@ -27,6 +26,13 @@ final class LocalFileReference extends Key
             $path = substr($path, 7);
         }
 
-        return new self(self::PATH_PREFIX . $path, $passphrase);
+        if (! file_exists($path)) {
+            throw FileCouldNotBeRead::onPath($path);
+        }
+
+        $key = new self('', $passphrase);
+        $key->content = self::PATH_PREFIX . $path;
+
+        return $key;
     }
 }
